bfa04a48d211de93917e0042a536778be1ca10d7
markoswythor <76796419+markoswythor@users.noreply.github.com>
2020-12-30 22:39:36 -0300

    Create help.js

---
+++ src/help.js
@@ -0,0 +1,41 @@
+const help = (prefix) => {
+	return `*Bem vindo ao Bot Sticker Bdr*\n
+Criadores:
+*MARKOS BOT TA*
+*MARKOS*\n
+💠 ʟɪsᴛᴀ ᴅᴇ ᴄᴏᴍᴀɴᴅᴏs 💠\n
+✅ *${prefix}info*: Mostra informações sobre o bot\n
+✅ *${prefix}sticker*: Transforma uma imagem, gif ou vídeo em sticker\n
+✅ *${prefix}toimg*: Converte um sticker em imagem
+_Use esse comando respondendo um sticker_\n
+✅ *${prefix}tts [lang] [texto]*: Converte texto em audio\n
+✅ *${prefix}neko*: Envia uma imagem de uma garota-gato aleatória\n
+✅ *${prefix}girl*: Envia uma imagem de uma garota aleatória\n
+✅ *${prefix}boy*: Envia uma imagem de um garoto aleatória\n
+✅ *${prefix}simg [texto]*: Pesquisa uma foto referente ao texto\n
+❌ *${prefix}chat [mensagem]*: O bot responde sua mensagem\n
+✅ *${prefix}wait*: Pesquisa o anime pela imagem\n
+✅ *${prefix}say [texto]*: O Bot repete o texto que foi mandado\n
+✅ *${prefix}tiktokstalk [usuário]*: Manda as informações da conta do usuário\n
+✅ *${prefix}lang*: Manda as linguagens disponiveis para o comando !tts\n
+💠 ɢʀᴜᴘᴏs 💠\n
+✅ *${prefix}linkgroup*: Manda o link de convite do grupo
+_Comando apenas para adms_\n
+✅ *${prefix}tagall*: Menciona todos do grupo
+_Comando apenas para adms_\n
+✅ *${prefix}listadmins*: Menciona todos os adms do grupo
+_Comando apenas para adms_\n
+✅ *${prefix}clone [tag]*: Copia a foto de perfil da pessoa marcada e adiciona no bot
+_Comando apenas para adms_\n
+✅ *${prefix}add [número]*: Adiciona o número ao grupo
+_Comando apenas para adms_\n
+✅ *${prefix}kick [tag]*: Remove a pessoa marcada
+_Comando apenas para adms_\n
+✅ *${prefix}promover [tag]*: Promove a pessoa marcada a adm
+_Comando apenas para adms_\n
+✅ *${prefix}rebaixar [tag]*: Tira o adm da pessoa marcada
+_Comando apenas para adms_`
+
+}
+
+exports.help = help
