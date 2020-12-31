9f3c4c79c28d49e3e9c27a8f2716d7da4cae429d
markoswythor <76796419+markoswythor@users.noreply.github.com>
2020-12-30 22:43:21 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+apt-get update
+apt-get upgrade
+apt-get install nodejs
+apt-get install libwebp
+apt-get install ffmpeg
+apt-get install wget
+apt-get install tesseract
+apt-get install python
+apt-get install pip
+pip install youtube_search
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm install image-to-base64
+npm install
+
+echo "[*] All dependencies have been installed, please run the command \"node index.js\" to immediately start the script"
